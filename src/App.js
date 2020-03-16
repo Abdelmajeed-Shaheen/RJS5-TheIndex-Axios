a76139a1e0d8c878b5f0a6d814acf7c70843ee27
Abdelmajeed <shaheen.abdelmajeed:outlook.com>
2020-03-16 12:38:56 +0200

    test_passed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React, { Component } from "react";
-
-import authors from "./data.js";
+import axios from "axios";
 
 // Components
 import Sidebar from "./Sidebar";
 import AuthorList from "./AuthorList";
+import Loading from "./Loading";
 import AuthorDetail from "./AuthorDetail";
 
 class App extends Component {
   state = {
-    currentAuthor: null
+    currentAuthor: null,
+    authors: [],
+    loading: true
   };
+  getauthors = async () => {
+    try {
+      const response = await axios.get(
+        "https://the-index-api.herokuapp.com/api/authors/"
+      );
+      const authors = response.data;
+      this.setState({
+        authors: authors,
+        loading: false
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  componentDidMount() {
+    this.getauthors();
+  }
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  selectAuthor = async authorID => {
+    this.setState({ loading: true });
+    try {
+      const response = await axios.get(
+        `https://the-index-api.herokuapp.com/api/authors/${authorID}/`
+      );
+      const author = response.data;
+      this.setState({
+        currentAuthor: author,
+        loading: false
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
   getContentView = () => {
-    if (this.state.currentAuthor) {
+    if (this.state.loading) {
+      return <Loading />;
+    } else if (this.state.currentAuthor) {
       return <AuthorDetail author={this.state.currentAuthor} />;
     } else {
-      return <AuthorList authors={authors} selectAuthor={this.selectAuthor} />;
+      return (
+        <AuthorList
+          authors={this.state.authors}
+          selectAuthor={this.selectAuthor}
+        />
+      );
     }
   };
 
---
+++ src/Loading.js
@@ -0,0 +1,8 @@
+import React from "react";
+const Loading = () => (
+  <div className=" mx-auto text-center">
+    <h3 style={{ color: "red" }}>loading</h3>
+  </div>
+);
+
+export default Loading;
